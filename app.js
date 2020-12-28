1d86bf4160c0e060ef40f7912d191bad3cb6c9c3
Admin-PC\Admin <kubar7kubar@gmail.com>
2020-12-28 22:15:47 +0100

    textarea input and output

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
-menu.addEventListener('click', function() {
+function navbar() {
 menu.classList.toggle('is-active')
 menuLinks.classList.toggle('active');
-})
+}
+menu.addEventListener('click', navbar);
+
+function output_from_textarea() {
+    var text = document.getElementById('inputjs').value;
+    document.getElementById('outputjs').innerHTML = text;
+}
+document.getElementById('submitjs').addEventListener('click', output_from_textarea);
